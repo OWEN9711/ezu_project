065c799f74b8e39eecf670f18306e793c6c2c7f3
Shajoun Zheng <shajoun3@illinois.edu>
2022-04-24 11:23:41 -0500

    ezu_setup

---
+++ courseinfo/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Semester, Section, Course, Instructor, Registration, Student, Period, Year
+
+admin.site.register(Semester)
+admin.site.register(Section)
+admin.site.register(Course)
+admin.site.register(Instructor)
+admin.site.register(Registration)
+admin.site.register(Student)
+admin.site.register(Period)
+admin.site.register(Year)
+
+
+
+
